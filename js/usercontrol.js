e302c5a65e34ca2e40aa3ea40eb2788fa42184df
uenowataru <ueno.wataru@live.com>
2015-04-26 21:07:44 -0400

    reorganized animation functions

--- js/usercontrol.js
+++ js/usercontrol.js
@@ -8,6 +8,8 @@ var ClickTime = 0;
 var ENABLE_VC = false;
 var DOUBLE_CLICK_THRESHOLD = 300;
 var TOUCH_DRAG_THRESHOLD = 100;
+var ALL_ANIMATION_TIME = 500;
+var ALL_FADE_TIME = 1000;
 var isTouchDevice = 'ontouchstart' in document.documentElement;
 var isMobile = (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
@@ -171,6 +173,9 @@ function animateAll(){
     animateTitle();
     animateArrow();
     animateTimeBar();
+    setTimeout(function(){
+      moved = false;
+    },ALL_ANIMATION_TIME);
     document.body.style.cursor = 'default';
   }else{
     clearTimeout(arrowtimeout);
@@ -179,25 +184,14 @@ function animateAll(){
     clearTimeout(cursortimeout);
     arrowtimeout = setTimeout(function(){
       if(isTouchDevice) return;
-      $( "#arrowL" ).animate({
-        opacity: 0
-      },1000);
-      $( "#arrowR" ).animate({
-        opacity: 0
-      },1000);
-      moved = false;
+      fadeElementById("#arrowL");
+      fadeElementById("#arrowR");
     }, 2000);
     titletimeout = setTimeout(function(){
-      $( "#title" ).animate({
-        opacity: 0
-      },1000);
+      fadeElementById("#title");
     }, 2000);
     timebartimeout = setTimeout(function(){
-      $( "#timebar" ).animate({
-        opacity: 0,
-        color: "#000"
-      }, 1000
-      );
+      fadeElementById("#timebar");
     }, 2000);
     cursortimeout = setTimeout(function(){
       document.body.style.cursor = 'none';
@@ -242,49 +236,49 @@ function loadTitle(){
 }
 
 function animateTitle(){
-  clearTimeout(titletimeout);
-  $( "#title" ).animate({
-    opacity: 1,
-  },500);
+  animateElementById("#title", 1.0);
   clearTimeout(titletimeout);
   titletimeout = setTimeout(function(){
-    $( "#title" ).animate({
-      opacity: 0
-    },1000);
+    fadeElementById("#title");
   }, 2000);
-
 }
 
 function animateArrow(){
   if(isTouchDevice) return;
-  $( "#arrowL" ).animate({
-    opacity: 0.5
-  },500);
-  $( "#arrowR" ).animate({
-    opacity: 0.5
-  },500);
+  animateElementById("#arrowL",0.5);
+  animateElementById("#arrowR",0.5);
   clearTimeout(arrowtimeout);
   arrowtimeout = setTimeout(function(){
-    $( "#arrowL" ).animate({
-      opacity: 0
-    },1000);
-    $( "#arrowR" ).animate({
-      opacity: 0
-    },1000);
+    fadeElementById("#arrowL");
+    fadeElementById("#arrowR");
   }, 2000);
 }
 
 
 function animateTimeBar(){
-  $( "#timebar" ).animate({
-    opacity: 0.75,
-  },500);
+  animateElementById("#timebar", 0.75);
   clearTimeout(timebartimeout);
   timebartimeout = setTimeout(function(){
-    $( "#timebar" ).animate({
-      opacity: 0,
-    },1000);
+    fadeElementById("#timebar");
   }, 2000);
+}
+
+function animateElementById(ElementID, TargetOpacity){
+  $( ElementID ).animate({
+    opacity: TargetOpacity,
+  }, {
+    queue: false,
+    duration: ALL_ANIMATION_TIME
+  });
+}
+
+function fadeElementById(ElementID){
+  $( ElementID ).animate({
+    opacity: 0,
+  }, {
+    queue: false,
+    duration: ALL_FADE_TIME
+  });
 }
 
 setInterval(function(){
